b1f7604e41453883daeea45830ad906158575813
Yan Jiang <jiangyan0302@gmail.com>
2021-06-29 17:20:58 +0800

    add modification of each mood in the dashboard

--- screens/entryScreen/EntryCard.js
+++ screens/entryScreen/EntryCard.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Text, View, SafeAreaView, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, ScrollView } from "react-native";
 import { globalStyles } from "../../styles/global";
 import { nanoid } from "nanoid/non-secure";
-import { Card, ListItem, Button, Avatar } from "react-native-elements";
+import { Card, ListItem, BottomSheet } from "react-native-elements";
 import { FontAwesome5 } from "@expo/vector-icons";
 
 import COLORS from "../../styles/colors";
@@ -19,6 +19,19 @@ export function getIconColor(mood) {
 }
 
 const EntryCard = ({ entryCardData }) => {
+  const [visible, setVisible] = useState(false);
+  const [date, setDate] = useState("");
+  const [mood, setMood] = useState("");
+  const openButtomSheet = (thedate, i) => {
+    setVisible(true);
+    setDate(thedate);
+    setMood(i);
+  };
+  const deleteMood = () => {
+    let a = newEntrydata.find((el) => el.date == date);
+    a.moodEntries.splice(mood, 1);
+    setVisible(false);
+  };
   //clean and reorg data
   let newEntrydata = [];
   for (let entry of entryCardData) {
@@ -44,29 +57,63 @@ const EntryCard = ({ entryCardData }) => {
 
   return (
     <ScrollView style={{ marginBottom: 100 }}>
-      {newEntrydata.map((card) => (
-        <Card key={nanoid()}>
-          <Card.Title>{card.date}</Card.Title>
-          <Card.Divider />
-          {card.moodEntries
-            .map((entry, i) => (
-              <ListItem key={i + entry.mood} bottomDivider>
-                <FontAwesome5
-                  name={entry.mood}
-                  size={ICONSIZE}
-                  color={getIconColor(entry.mood)}
-                />
-                <ListItem.Content>
-                  <ListItem.Title>{entry.mood}</ListItem.Title>
-                  <ListItem.Subtitle>{entry.label}</ListItem.Subtitle>
-                  <ListItem.Subtitle>{entry.note}</ListItem.Subtitle>
-                </ListItem.Content>
-              </ListItem>
-            ))
-            .reverse()}
-        </Card>
-      ))}
+      {newEntrydata
+        .filter((el) => el.moodEntries.length > 0)
+        .map((card) => (
+          <Card key={nanoid()}>
+            <Card.Title>{card.date}</Card.Title>
+            <Card.Divider />
+            {card.moodEntries
+              .map((entry, i) => (
+                <ListItem key={i + entry.mood} bottomDivider>
+                  <FontAwesome5
+                    name={entry.mood}
+                    size={ICONSIZE}
+                    color={getIconColor(entry.mood)}
+                  />
+                  <ListItem.Content>
+                    <ListItem.Title>{entry.mood}</ListItem.Title>
+                    <ListItem.Subtitle>{entry.label}</ListItem.Subtitle>
+                    <ListItem.Subtitle>{entry.note}</ListItem.Subtitle>
+                  </ListItem.Content>
+                  <ListItem.Chevron
+                    onPress={() => openButtomSheet(card.date, i)}
+                  />
+                </ListItem>
+              ))
+              .reverse()}
+          </Card>
+        ))
+        .reverse()}
+      <BottomSheet
+        isVisible={visible}
+        onBackdropPress={() => setVisible(false)}
+        containerStyle={{ backgroundColor: "rgba(0.5, 0.25, 0, 0.2)" }}
+      >
+        <ListItem bottomDivider>
+          <ListItem.Content>
+            <ListItem.Title style={styles.text}>Edit</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+        <ListItem bottomDivider onPress={() => deleteMood()}>
+          <ListItem.Content>
+            <ListItem.Title style={styles.text}>Delete</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+        <ListItem bottomDivider onPress={() => setVisible(false)}>
+          <ListItem.Content>
+            <ListItem.Title style={styles.text}>Cancel</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+      </BottomSheet>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    alignSelf: "center",
+  },
+});
 export default EntryCard;
